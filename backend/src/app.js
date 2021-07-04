41fc8b6b026ef9bead15236317720be1a8c0c2a6
Cristian Camilo <tiancamilo59@gmail.com>
2021-07-04 03:34:43 -0500

    Se agrego modulo cors, morgan, se agregaron variables de entorno y se cambio ubicacion del archivo request

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,8 +1,12 @@
 //Configuración de express
 import express from 'express';
+import cors from 'cors';
+import morgan from 'morgan';
 import tasksRoutes from './routes/tasks'
 
 const app = express();
+app.use(cors());
+app.use(morgan('dev'));
 app.use(express.json());
 app.use(tasksRoutes)
 
--- backend/src/config.js
+++ backend/src/config.js
@@ -1,6 +1,8 @@
+import {config as dotenv } from 'dotenv'
+dotenv()
 export const config = {
-    host: 'localhost',
-    user: 'root',
-    password: 'root',
-    database: 'tasksdb'
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_DATABASE || 'test'
 }
