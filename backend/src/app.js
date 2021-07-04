31c5ba13ae95a828f0e9c774f9c0cd3dedb554d6
Cristian Camilo <tiancamilo59@gmail.com>
2021-07-04 03:12:08 -0500

    Se finalizo funcionalidad de las demas rutas del api

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,7 @@ import express from 'express';
 import tasksRoutes from './routes/tasks'
 
 const app = express();
+app.use(express.json());
 app.use(tasksRoutes)
 
 export default app 
--- backend/src/controllers/tasks.js
+++ backend/src/controllers/tasks.js
@@ -9,25 +9,44 @@ export const getTasks = async (req, res) =>{
 
 export const getTask = async (req, res) =>{
     const connection = await connect()
-    const [rows] = await connection.query("SELECT * FROM task  WHERE id = ?", [req.params.id,]);
-    console.log(rows[0])
-    res.send('Hello World!!')
+    const [rows] = await connection.query("SELECT * FROM task  WHERE id = ?", [req.params.id,])
+    res.json(rows[0])
 }
 
-export const getTasksCount = (req, res) =>{
-    res.send('Hello World!!')
+export const getTasksCount = async (req, res) =>{
+    const connection = await connect()
+    const [rows] = await connection.query("SELECT COUNT(*) FROM task")
+    res.json(rows[0]["COUNT(*)"])
 }
 
-export const saveTask = (req, res) =>{
-    res.send('Hello World!!')
+export const saveTask = async (req, res) =>{
+    const connection = await connect()
+    const [results] = await connection.query("INSERT INTO task(title, description) VALUES (?,?)",[
+        req.body.title,
+        req.body.description
+    ])
+    res.json({
+        id: results.insertId,
+        ...req.body,
+
+    })
 }
 
-export const deleteTask = (req, res) =>{
-    res.send('Hello World!!')
+export const deleteTask = async (req, res) =>{
+    const connection = await connect()
+    await connection.query("DELETE FROM task WHERE id = ?",[req.params.id])
+    res.sendStatus(204)
 }
 
-export const updateTask = (req, res) =>{
-    res.send('Hello World!!')
+export const updateTask = async (req, res) =>{
+    const connection = await connect()
+    const results = await connection.query("UPDATE task SET ? WHERE id = ?",[
+        req.body,
+        req.params.id
+    ])
+    console.log(results);
+    res.sendStatus(204)
+
 }
 
 
