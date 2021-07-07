e0e3ec142bbd5e0bc1dbf07a1e8181fe05a12adc
Cristian Camilo <tiancamilo59@gmail.com>
2021-07-06 21:27:02 -0500

    Se configuro swagger para realizar la documentascion del API

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,13 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
+
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express';
+import {options} from './swaggerOptions';
+
+const spec = swaggerJSDoc(options)
+
 import tasksRoutes from './routes/tasks'
 
 const app = express();
@@ -9,5 +16,7 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(tasksRoutes)
+
+app.use('/docs', swaggerUI.serve, swaggerUI.setup(spec));
 
 export default app 
--- backend/src/routes/tasks.js
+++ backend/src/routes/tasks.js
@@ -3,16 +3,72 @@ import { Router } from 'express';
 import { deleteTask, getTask, getTasks, getTasksCount, saveTask, updateTask } from '../controllers/tasks'
 const router = Router()
 
+/**
+ * @swagger
+ * tags:
+ *  name: Tasks
+ *  description: Tasks endpoint 
+ *  
+ */
+
+/**
+ * @swagger
+ * /tasks:
+ *  get:
+ *   summary: Get all tasks
+ *   tags: [Tasks]
+ *   
+ */
 router.get('/tasks', getTasks)
 
+/**
+ * @swagger
+ * /tasks/count:
+ *  get:
+ *   summary: Get total tasks counter
+ *   tags: [Tasks]
+ *   
+ */
 router.get('/tasks/count', getTasksCount)
 
+/**
+ * @swagger
+ * /tasks/:id:
+ *  get:
+ *   summary: Get a task by id
+ *   tags: [Tasks]
+ *   
+ */
 router.get('/tasks/:id', getTask)
 
+/**
+ * @swagger
+ * /tasks/:id:
+ *  post:
+ *   summary: Save a new task
+ *   tags: [Tasks]
+ *   
+ */
 router.post('/tasks', saveTask)
 
+/**
+ * @swagger
+ * /tasks/:id:
+ *  delete:
+ *   summary: Delete a task by id
+ *   tags: [Tasks]
+ *   
+ */
 router.delete('/tasks/:id', deleteTask)
 
+/**
+ * @swagger
+ * /tasks/:id:
+ *  put:
+ *   summary: Update a task by id
+ *   tags: [Tasks]
+ *   
+ */
 router.put('/tasks/:id', updateTask)
 
 
